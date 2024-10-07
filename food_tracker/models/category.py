070bbb498d77eaa58aed118cfeb3e42e70ec9451
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-10-07 22:55:55 +0300

    created Category model

--- food_tracker/models/category.py
+++ food_tracker/models/category.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        db_table = 'food_category'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
