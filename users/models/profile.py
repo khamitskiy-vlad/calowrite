d1bab3805074ad8708145d78f43c6ca4e933479d
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-10-07 22:25:49 +0300

    created profile model for default user

--- users/models/profile.py
+++ users/models/profile.py
@@ -1,1 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.validators import MinValueValidator
+
+class Profile(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True) 
+    weight = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
+    height = models.IntegerField(blank=True, validators=[MinValueValidator(0)])
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+
+def __str__(self):
+    return f'{self.user.username} Profile'
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
