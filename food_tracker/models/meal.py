dd6341b740cc4a1058d7bfc82baa1cc5ae0f64c7
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-10-08 00:13:19 +0300

    created meal model

--- food_tracker/models/meal.py
+++ food_tracker/models/meal.py
@@ -1,1 +1,16 @@
 from django.db import models
+from .diary import Diary
+from .meal_type import MealType
+from django.core.validators import MinValueValidator
+
+class Meal(models.Model):
+    diary = models.ForeignKey(Diary, related_name='meals', on_delete=models.CASCADE)
+    meal_type = models.ForeignKey(MealType, on_delete=models.CASCADE)
+    calories = models.FloatField(validators=[MinValueValidator(0)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'{self.meal_type.name()} - {self.calories} kcal'
